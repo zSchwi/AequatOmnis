cdfb98792866adee1827208f75f757b7d9813808
Guilherme Fabricio <guilherme.fabricio.115@gmail.com>
2023-07-15 01:01:45 -0300

    merge do front com o back

---
+++ frontend_vuejs/src/views/CadastrarProdutoView.vue
@@ -0,0 +1,84 @@
+<script setup>
+import { reactive } from 'vue';
+
+const dadosProduto = reactive({
+    nome: "",
+    empresa: "",
+    genero: "",
+    plataforma: "",
+    valor: ""
+
+})
+
+const enviarDadosProduto = async () => {
+    try {
+        const { data } = await http.post('/jogos', dadosProduto)
+        console.log(data)
+    } catch (error) {
+        console.log(error?.response?.data)
+    }
+}
+
+</script>
+
+
+
+
+
+<template>
+    <v-main>
+        <v-container>
+            <v-card>
+
+                <v-form @submit.prevent="enviarDadosProduto">
+                    <v-col cols="12">
+                        <v-container>
+                            <v-row class="d-flex justify-center mb-5">
+                                <h3>Cadastro de Produto</h3>
+                            </v-row>
+                            <v-row>
+                                <v-col col="9" md="9">
+                                    <v-text-field v-model="dadosProduto.nome" label="Nome completo:"
+                                        required></v-text-field>
+                                </v-col>
+                            </v-row>
+
+                            <v-row>
+
+                                <v-col cols="5" md="3">
+                                    <v-text-field type="text" v-model="dadosProduto.empresa" label="Empresa:"
+                                        required></v-text-field>
+                                </v-col>
+
+                                <v-col cols="5" md="3">
+                                    <v-text-field type="text" v-model="dadosProduto.genero" label="Genero:"
+                                        required></v-text-field>
+                                </v-col>
+
+                                <v-col cols="5" md="3">
+                                    <v-text-field type="text" v-model="dadosProduto.plataforma" label="Plataforma"
+                                        required></v-text-field>
+                                </v-col>
+
+                            </v-row>
+
+                            <v-row>
+                                <v-col cols="5" md="3">
+                                    <v-text-field type="text" v-model="dadosProduto.valor" label="Valor:"
+                                        required></v-text-field>
+                                </v-col>
+                            </v-row>
+
+                            <v-row class="justify-end">
+                                <v-btn type="submit" color="green-darken-1" class="mx-5">Confirmar</v-btn>
+                                <v-btn color="red-darken-1">Cancelar</v-btn>
+                            </v-row>
+                        </v-container>
+                    </v-col>
+
+
+                </v-form>
+            </v-card>
+        </v-container>
+    </v-main>
+</template>
---
+++ frontend_vuejs/src/views/TabelaVendasView.vue
@@ -0,0 +1,97 @@
+<template>
+    <v-table class="">
+        <thead>
+            <tr>
+                <th class="text-left">
+                    Nome Produto
+                </th>
+                <th class="text-left">
+                    Comprador
+                </th>
+
+                <th class="text-left">
+                    Genero
+                </th>
+
+                <th class="text-left">
+                    Plataforma
+                </th>
+
+                <th class="text-left">
+                    Valor
+                </th>
+
+                <th class="text-right">
+                    Operações
+                </th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr v-for="(jogo, i) in jogos" :key="i">
+                <td>{{ jogo.nome }}</td>
+                <td>{{ jogo.empresa }}</td>
+                <td>{{ jogo.genero }}</td>
+                <td>{{ jogo.plataforma }}</td>
+                <td>{{ jogo.valor }}</td>
+                <td class="d-flex justify-end">
+                    <v-btn icon="mdi-update"></v-btn>
+                    <v-btn icon="mdi-delete"></v-btn>
+                    <v-btn icon="mdi-pencil"></v-btn>
+                </td>
+            </tr>
+        </tbody>
+    </v-table>
+
+
+
+    <div class="d-flex justify-end">
+        <v-btn class="my-6 mx-5" color="green-darken-2">Cadastrar</v-btn>
+    </div>
+</template>
+
+
+<script setup>
+import { ref } from 'vue';
+
+const jogos = ref([
+    {
+        nome: 'Dark Souls',
+        empresa: 'essa daqui',
+        genero: 'SoulsLike',
+        plataforma: 'PC',
+        valor: 35
+    },
+
+    {
+        nome: 'Fallout 4',
+        empresa: 'essa daqui',
+        genero: 'SoulsLike',
+        plataforma: 'PC',
+        valor: 35
+    },
+
+    {
+        nome: 'Skyrim',
+        empresa: 'essa daqui',
+        genero: 'SoulsLike',
+        plataforma: 'PC',
+        valor: 35
+    },
+
+    {
+        nome: 'Battlefield 4',
+        empresa: 'essa daqui',
+        genero: 'SoulsLike',
+        plataforma: 'PC',
+        valor: 35
+    },
+
+    {
+        nome: 'Forza Horizon',
+        empresa: 'essa daqui',
+        genero: 'SoulsLike',
+        plataforma: 'PC',
+        valor: 35
+    }
+])
+</script>
